1e646eae5f98eaf8cf11a3efe10bf0ec29378a34
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-04-11 13:33:53 +0300

    add components
    (cherry picked from commit e5eda704388eb26bbe241265a9312a522385a7ae)

---
+++ homeworks/01-bubble-sort/src/components/Frame/index.ts
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class Frame extends Component {
+  render() {
+    return null;
+  }
+}
+
+export default Frame;
---
+++ homeworks/01-bubble-sort/src/components/Title/index.ts
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class Title extends Component {
+  render() {
+    return null;
+  }
+}
+
+export default Title;
