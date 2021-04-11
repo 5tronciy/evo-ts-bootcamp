bb8f3bdfa099ce17779654ca83f4e670d686ef10
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-04-11 13:58:47 +0300

    fulfill Frame component

---
+++ homeworks/01-bubble-sort/src/components/Frame/index.tsx
@@ -0,0 +1,16 @@
+const Frame = () => {
+  return (
+    <>
+      <div className="array" style={{ height: "200px" }}>
+        <div className="element" style={{ height: "86px" }}></div>
+        <div className="element" style={{ height: "38px" }}></div>
+      </div>
+      <div className="buttons">
+        <button>New set</button>
+        <button>Start</button>
+      </div>
+    </>
+  );
+};
+
+export default Frame;
