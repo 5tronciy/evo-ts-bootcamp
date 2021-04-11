e5eda704388eb26bbe241265a9312a522385a7ae
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-04-11 13:33:53 +0300

    add components

--- homeworks/01-bubble-sort/src/App.tsx
+++ homeworks/01-bubble-sort/src/App.tsx
@@ -1,8 +1,28 @@
 import { Component } from "react";
+import Title from "./components/Title";
+import Frame from "./components/Frame";
+import Status from "./components/Status";
+
+type Props = {};
 
 class App extends Component {
+  constructor(props: Props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
   render() {
-    return null;
+    return (
+      <>
+        <Title />
+        <Frame />
+        <Status />
+      </>
+    );
   }
 }
 
---
+++ homeworks/01-bubble-sort/src/components/Status/index.ts
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class Status extends Component {
+  render() {
+    return null;
+  }
+}
+
+export default Status;
