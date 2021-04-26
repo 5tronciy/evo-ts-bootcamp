16506c5b070306aefa0b089aabbbfbc7cbc1ba91
5tronciy <gorovin-ne@ya.ru>
2021-04-26 16:54:57 +0300

    add redux, init store

--- homeworks/05-redux-basic/src/App.tsx
+++ homeworks/05-redux-basic/src/App.tsx
@@ -1,5 +1,48 @@
-import "./App.css";
+import { useEffect } from "react";
+import { createStore, Reducer } from "redux";
+
+type ActionTypes = any;
+
+export const amountReducer: Reducer<number, ActionTypes> = (
+  state = 0,
+  action
+) => {
+  switch (action.type) {
+    case "UPDATE_BALANCE":
+      return action.payload;
+    case "DEBIT":
+      return state - action.payload;
+    case "CREDIT":
+      return state + action.payload;
+    case "GET_BALANCE_WITHOUT_TAX":
+      return (state * (100 - action.payload)) / 100;
+    default:
+      return state;
+  }
+};
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__: any;
+  }
+}
+
+const store = createStore(
+  amountReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const array = [
+  { type: "UPDATE_BALANCE", payload: 1000.0 },
+  { type: "CREDIT", payload: 200.0 },
+  { type: "CREDIT", payload: 100.0 },
+  { type: "GET_BALANCE_WITHOUT_TAX", payload: 14 },
+  { type: "DEBIT", payload: 250.0 },
+  { type: "UPDATE_BALANCE", payload: 1000.0 },
+];
 
 export const App = () => {
+  useEffect(() => array.forEach((action) => store.dispatch(action), []));
+
   return <></>;
 };
