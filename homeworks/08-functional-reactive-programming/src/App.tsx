ac47e4baa7a7d2d5fb1917f9b136e5216277724e
5tronciy <gorovin-ne@ya.ru>
2021-06-29 16:48:51 +0300

    add "rxjs", use timer from "rxjs" instead of setInterval

--- homeworks/08-functional-reactive-programming/src/App.tsx
+++ homeworks/08-functional-reactive-programming/src/App.tsx
@@ -2,14 +2,18 @@ import { useCallback, useEffect, useState } from "react";
 import s from "./App.module.css";
 import { House } from "./House";
 import { ReactComponent as Cat } from "./assets/cat.svg";
+import { timer } from "rxjs";
 
 interface Coordinates {
   top: number;
   left: number;
 }
 
+const observable$ = timer(0, 1000);
+
 function App() {
   const [score, setScore] = useState(0);
+  // const [windowWithCat, setWindowWithCat] = useState(0);
 
   const selectCat = useCallback(() => document.getElementById("cat"), []);
 
@@ -20,6 +24,18 @@ function App() {
     return windowsCoordinates;
   }, []);
 
+  useEffect(() => {
+    const windows = getWindowsCoords();
+    let subscription = observable$.subscribe(() => {
+      const windowWithCat = Math.floor(Math.random() * windows.length);
+      const cat: HTMLElement | null = selectCat();
+      cat!.style.display = "block";
+      cat!.style.left = windows[windowWithCat].left.toString();
+      cat!.style.top = windows[windowWithCat].top.toString();
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+
   function getCoords(elem: Element) {
     var box = elem.getBoundingClientRect();
     return {
@@ -27,18 +43,6 @@ function App() {
       left: box.left,
     };
   }
-
-  useEffect(() => {
-    let timerId: null | ReturnType<typeof setInterval> = setInterval(() => {
-      const windows = getWindowsCoords();
-      const windowWithCat = Math.floor(Math.random() * windows.length);
-      const cat: HTMLElement | null = selectCat();
-      cat!.style.display = "block";
-      cat!.style.left = windows[windowWithCat].left.toString();
-      cat!.style.top = windows[windowWithCat].top.toString();
-    }, 1000);
-    return () => clearInterval(timerId!);
-  }, [getWindowsCoords, selectCat]);
 
   const onCat = () => {
     const cat: HTMLElement | null = selectCat();
