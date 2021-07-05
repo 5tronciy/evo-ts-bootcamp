8bb0cfb921d656ae020d4657c04bd99330903da6
5tronciy <gorovin-ne@ya.ru>
2021-07-05 16:57:32 +0300

    refactor render of House, attempt to calculate the score via fromEvent observable

--- homeworks/08-functional-reactive-programming/src/App.tsx
+++ homeworks/08-functional-reactive-programming/src/App.tsx
@@ -1,22 +1,43 @@
-import { useCallback, useEffect, useState } from "react";
+import {
+  useCallback,
+  useEffect,
+  useState,
+  Dispatch,
+  SetStateAction,
+} from "react";
 import s from "./App.module.css";
 import { House } from "./House";
 import { ReactComponent as Cat } from "./assets/cat.svg";
-import { timer } from "rxjs";
+import { timer, Observable, fromEvent } from "rxjs";
+import { scan } from "rxjs/operators";
 
 interface Coordinates {
   top: number;
   left: number;
 }
 
+const useObservable = (
+  observable: Observable<any>,
+  setter: Dispatch<SetStateAction<any>>
+) => {
+  useEffect(() => {
+    let subscribtion = observable.subscribe((result: any) => setter(result));
+    return () => subscribtion.unsubscribe();
+  }, [observable, setter]);
+};
+
 const observable$ = timer(0, 1000);
 
 function App() {
   const [score, setScore] = useState(0);
-  // const [windowWithCat, setWindowWithCat] = useState(0);
 
   const selectCat = useCallback(() => document.getElementById("cat"), []);
-
+  // const cat = document.getElementById("cat")!;
+  const target = document;
+  const clickOnCat$ = fromEvent(target, "click").pipe(
+    scan((count) => count + 1, 0)
+  );
+  useObservable(clickOnCat$, setScore);
   const getWindowsCoords = useCallback(() => {
     const windows = document.querySelectorAll(".window");
     const windowsCoordinates: Coordinates[] = [];
@@ -28,13 +49,13 @@ function App() {
     const windows = getWindowsCoords();
     let subscription = observable$.subscribe(() => {
       const windowWithCat = Math.floor(Math.random() * windows.length);
-      const cat: HTMLElement | null = selectCat();
+      const cat: any = selectCat();
       cat!.style.display = "block";
       cat!.style.left = windows[windowWithCat].left.toString();
       cat!.style.top = windows[windowWithCat].top.toString();
     });
     return () => subscription.unsubscribe();
-  }, []);
+  }, [getWindowsCoords, selectCat]);
 
   function getCoords(elem: Element) {
     var box = elem.getBoundingClientRect();
@@ -45,7 +66,7 @@ function App() {
   }
 
   const onCat = () => {
-    const cat: HTMLElement | null = selectCat();
+    const cat: any = selectCat();
     setScore(score + 1);
     cat!.style.display = "none";
   };
